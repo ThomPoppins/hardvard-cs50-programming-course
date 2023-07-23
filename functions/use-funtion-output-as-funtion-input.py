04fa1be45f6d527738b9fe4f518411551d24bb20
Thom Poppins <thompoppins@gmail.com>
2023-07-23 16:21:31 +0100

    use function output as function input

---
+++ functions/use-funtion-output-as-funtion-input.py
@@ -0,0 +1,3 @@
+# put the output of input as a argument into the int()
+# and print() the output of the sum of the two int() functions
+print(int(input("What is x?: ")) + int(input("What is y?: ")))
--- integers/calculator.py
+++ integers/calculator.py
@@ -2,8 +2,8 @@
 # format the output to data type int by using int()
 # this way you put the output of input as a argument into the int() function and assign it to a variable
 """
-x = int(input("What is x?: "))
-y = int(input("What is y?: "))
+# x = int(input("What is x?: "))
+# y = int(input("What is y?: "))
 
 # z = x + y
 
@@ -16,4 +16,7 @@ don't create a variable if you don't need it
 prevent creating the z variable by using the + operator inside the print function
 """
 
-print(f"{x} + {y} = {x + y}")
+# print(f"{x} + {y} = {x + y}")
+
+#you can even do this:
+print(int(input("What is x?: ")) + int(input("What is y?: ")))
