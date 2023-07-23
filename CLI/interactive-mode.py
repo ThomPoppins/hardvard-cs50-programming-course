e638ac90f0852fc726b7b9f0b52d6cac47194413
Thom Poppins <thompoppins@gmail.com>
2023-07-23 15:43:19 +0100

    calculator

---
+++ CLI/interactive-mode.py
@@ -0,0 +1,9 @@
+# open python interactive mode inside the CLI
+# enter 1 + 1 and press enter, you will get 2
+
+# python will execute the code line by line after you press enter
+
+# you can also use the python interactive mode to test your code
+# before you put it in your file
+
+# to exit the interactive mode, type exit() and press enter
---
+++ integers/calculator.py
@@ -0,0 +1,19 @@
+"""
+# format the output to data type int by using int()
+# this way you put the output of input as a argument into the int() function and assign it to a variable
+"""
+x = int(input("What is x?: "))
+y = int(input("What is y?: "))
+
+# z = x + y
+
+# print(f"{x} + {y} = {z}")
+
+"""
+Keep your code as simple as possible,
+don't create a variable if you don't need it
+
+prevent creating the z variable by using the + operator inside the print function
+"""
+
+print(f"{x} + {y} = {x + y}")
