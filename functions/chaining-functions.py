1a5a1f189dc7f667fb435a04764cea1ebde60ab3
Thom Poppins <thompoppins@gmail.com>
2023-07-23 15:22:14 +0100

    split strings

--- functions/chaining-functions.py
+++ functions/chaining-functions.py
@@ -7,13 +7,13 @@ name = input("What is your name?: ")
 # capitalize str (first letter)
 # name = name.capitalize()
 
-# chaining the formatting functions is a good way to keep the code clean and readable
-# the order of the functions is important, if you capitalize the name first and then remove the whitespace, the name will not be capitalized
-# the order of the functions is from left to right
-# capitalize letters from all words in str
-# name = name.title()
+"""
+chaining the formatting functions is a good way to keep the code clean and readable
+the order of the functions is important, if you capitalize the name first and then remove the whitespace, the name will not be capitalized
+the order of the functions is from left to right, so the first function will be executed first
+"""
 
-# chain the functions in order to remove whitespace and then capitalize all letters from all words in the name
+# chain the functions in order to remove whitespace in the front and back of the string and then capitalize all letters from all words in the name
 name = name.strip().title()
 
 # Say hello to the user
@@ -21,3 +21,6 @@ print(f"Hello {name}")
 
 # HINT: chain the formatting functions to the first input() function so you have your name formatted correctly from the start
 name = input("What is your name?: ").strip().title()
+
+# Say hello to the user
+print(f"Hello {name}")
--- strings/strip-string-from-whitespaces.py
+++ strings/strip-string-from-whitespaces.py
@@ -1,6 +1,6 @@
 name = input("What is your name?: ")
 
-# Remove whitespace from str
+# Remove whitespace from the beginning and end of the string
 name = name.strip()
 
 # Say hello to the user
